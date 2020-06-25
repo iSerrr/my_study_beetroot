aa9888aee6fbd5e68ca9c6674469b8146e970998
iSerrr <tsiutsiak.l@gmail.com>
2020-06-25 12:02:59 +0300

    H28 ready 3 of 3

--- js/main.js
+++ js/main.js
@@ -1,250 +1,143 @@
-//? 1. Создать массив «Список покупок». Каждый элемент массива является объектом, который содержит название продукта, необходимое количество и куплен или нет. Написать несколько функций для работы с таким массивом.
+//? 1) Реализовать класс, описывающий окружность. В классе должны быть следующие компоненты:
 
-let item1 = {
-  name: "Apple",
-  amount: 5,
-  buy: "no",
-}
-let item2 = {
-  name: "Banana",
-  amount: 2,
-  buy: "yes",
-}
-let item3 = {
-  name: "Kiwi",
-  amount: 8,
-  buy: "no",
-}
-let item4 = {
-  name: "Orange",
-  amount: 1,
-  buy: "no",
-}
+class Circle {
 
-let shopingList = [item1, item2, item3, item4];
+  constructor(options) {
+    this.radius = options.radius
+  }
 
-//todo Вывод всего списка на экран таким образом, чтобы сначала шли некупленные продукты, а потом – купленные.
-function newlist(arr) {
-  let newArr = [];
-  arr.forEach(item => item.buy === "no" ? newArr.unshift(item) : newArr.push(item));
-  return newArr;
+  get showRadius() {
+    return this.radius + ' см'
+  }
+  get showDiamert() {
+    return this.radius * 2 + ' см'
+  }
 
-};
-console.table(newlist(shopingList));
-
-//todo Добавление покупки в список. Учтите, что при добавлении покупки с уже существующим в списке продуктом, необходимо увеличивать количество в существующей покупке, а не добавлять новую.
-function addItem(newItem, arr, num = 1) {
-  let i = 0;
-  arr.forEach((item) => {
-    // newItem === item.name ? item.amount += num : item.amount + 0;
-    if (newItem === item.name) {
-      i++;
-      return item.amount += num;
-    }
-
-  })
-  if (i === 0) { arr.push({ name: newItem, amount: num, buy: "no" }) };
-}
-//todo Покупка продукта. Функция принимает название продукта и отмечает его как купленный.
-function toB(itemB, arr) {
-  arr.forEach(function (item,) {
-    if (itemB === item.name) item.buy = "Yes";
-  })
+  set newRadius(newValue) {
+    this.radius = newValue;
+  }
+  area() {
+    return Math.ceil(Math.PI * (this.radius ** 2)) + ' см'
+  }
+  length() {
+    return Math.ceil(this.radius * Math.PI * 2) + ' см'
+  }
 }
 
-console.log("Завдання 1.1")
-console.log(newlist(shopingList));
+let circle = new Circle({
+  radius: 10
+});
 
-console.log("Завдання 1.2 (+Potato 1x)")
-addItem("Potato", shopingList);
-console.log(shopingList);
+document.write(`Круг із радіусом ${circle.showRadius}<br>діаметр - ${circle.showDiamert}<br>площа круга - ${circle.area()}<br>довжина круга - ${circle.length()}`);
+document.write(`<p>Задамо новий радіус 25</p>`);
+circle.newRadius = 25;
+document.write(`Круг із радіусом ${circle.showRadius}<br>діаметр - ${circle.showDiamert}<br>площа круга - ${circle.area()}<br>довжина круга - ${circle.length()}`);
 
-console.log("Завдання 1.3")
-toB('Potato', shopingList);
-console.log(shopingList);
+// поле, хранящее радиус окружности;
+// get-свойство, возвращающее радиус окружности;
+// set-свойство, устанавливающее радиус окружности;
+// get-свойство, возвращающее диаметр окружности;
+// метод, вычисляющий площадь окружности;
+// метод, вычисляющий длину окружности.
+// Продемонстрировать работу свойств и методов. 
 
-//? 2. Создать массив, описывающий чек в магазине. Каждый элемент массива состоит из названия товара, количества и цены за единицу товара. Написать следующие функции:
-let receipt = [
-  {
-    name: "Battery",
-    amount: 2,
-    price: 10.50,
-  },
-  {
-    name: "Lantern",
-    amount: 1,
-    price: 35,
-  },
-  {
-    name: "Pan",
-    amount: 4,
-    price: 4,
-  },
-  {
-    name: "Box",
-    amount: 5,
-    price: 25,
-  },
-]
+//? 2) Реализовать класс, описывающий простой маркер. В классе должны быть следующие компоненты:
+class Marker {
+  constructor(options) {
+    this.color = options.color;
+    this.volume = options.volume;
+  }
 
-//todo Распечатка чека на экран;
-function show(arr) {
-  arr.forEach((item) => console.log(`${item.name} 1x${item.price} ${item.amount * item.price}`))
+  print(text) {
+    document.write(`<p style="color:${this.color};">${text.slice(0, this.volume / 0.5)}</p>`);
+  }
 }
-show(receipt);
 
-//todo Подсчет общей суммы покупки;
-function sum(arr) {
-  return arr.reduce((sum, item) => sum += (item.amount * item.price), 0)
+let marker = new Marker({
+  color: 'red',
+  volume: 25
+})
+console.log(marker);
+marker.print('Lorem ipsum dolor, sit amet consectetur adipisicing elit.Laudantium, eos vel ipsum quo optio beatae vero nobis sunt delectus est aliquid et rerum reprehenderit numquam tenetur repellendus quaerat, ullam debitis ?');
+
+class MarkerRepair extends Marker {
+  constructor(options) {
+    super(options)
+  }
+  repair(vol) {
+    this.volume = vol;
+  }
 }
-console.log(sum(receipt));
+let marker2 = new MarkerRepair({
+  color: "green",
+  volume: 45
+})
+marker2.print('Lorem ipsum dolor, sit amet consectetur adipisicing elit.Laudantium, eos vel ipsum quo optio beatae vero nobis sunt delectus est aliquid et rerum reprehenderit numquam tenetur repellendus quaerat, ullam debitis ?');
+marker2.repair(85);
+marker2.print('Перезапрвали і далі -- Lorem ipsum dolor, sit amet consectetur adipisicing elit.Laudantium, eos vel ipsum quo optio beatae vero nobis sunt delectus est aliquid et rerum reprehenderit numquam tenetur repellendus quaerat, ullam debitis ?');
 
-//todo Получение самой дорогой покупки в чеке;
-function maxPrice(arr) {
-  return arr.reduce((maxV, item) => maxV <= item.price ? maxV = item.price : maxV + 0, 0)
+// поле, которое хранит цвет маркера;
+// поле, которое хранит количество чернил в маркере (в процентах);
+// метод для печати (метод принимает строку и выводит текст соответствующим цветом; текст выводится до тех пор, пока в маркере есть чернила; один не пробельный символ – это 0,5% чернил в маркере).
+// Реализовать класс, описывающий заправляющийся маркер, унаследовав его от простого маркера и добавив метод для заправки маркера.
+// Продемонстрировать работу написанных методов. 
+
+//? 3) Реализовать класс Employee, описывающий работника, и создать массив работников банка.
+
+class Employee {
+  constructor(options) {
+    this.name = options.name;
+    this.age = options.age;
+    this.exp = options.exp;
+  }
 }
-console.log(maxPrice(receipt));
+let employee01 = new Employee({
+  name: 'John',
+  age: 35,
+  exp: 7
+});
+let employee02 = new Employee({
+  name: 'Sarra',
+  age: 31,
+  exp: 5
+});
+let employee03 = new Employee({
+  name: 'Mike',
+  age: 39,
+  exp: 11
+});
+let employee04 = new Employee({
+  name: 'Jimmy',
+  age: 28,
+  exp: 2
+});
+let employee05 = new Employee({
+  name: 'Anna',
+  age: 27,
+  exp: 1
+});
 
-//todo Подсчет средней стоимости одного товара в чеке.
-function averagePrice(arr) {
-  // version 1 (хоч тут і застосовано 2 reduce, але мені він більше доподоби)
-  return arr.reduce((sum, item,) => sum + item.price, 0) / (1 + arr.reduce((sum, item, i) => i++));
+let EmployeeOfBank = [employee01, employee02, employee03, employee04, employee05];
+console.table(EmployeeOfBank);
 
-  // version 2
-  // let sum = 0;
-  // for (let i = 0; i <ol arr.length;) {
-  //   sum += arr[i].price;
-  //   i++;
-  //   if (i === arr.length) { return sum / i };
-  // }
+//Реализовать класс EmpTable для генерации HTML-кода таблицы со списком работников банка. Массив работников необходимо передавать через конструктор, а получать HTML-код с помощью метода getHtml().
+
+class EmpTable {
+  constructor(arr) {
+    this.arr = arr;
+  }
+
+  get getHtml() {
+    document.write(`<table class="table">`);
+    document.write(`<tr><th>Ім'я</th><th>Вік</th><th>Досвід</th></tr>`);
+    this.arr.forEach(element => {
+      document.write(`<tr><td>${element.name}</td><td>${element.age}</td><td>${element.exp}</td></tr>`);
+    });
+    document.write(`</table>`)
+  }
 }
-console.log(`Завдання 2.4\n ${averagePrice(receipt)}`);
 
-//? 3. Создать массив CSS-стилей (цвет, размер шрифта, выравнивание, подчеркивание и т. д.). Каждый элемент массива – это объект, состоящий из двух свойств: название стиля и значение стиля. Написать функцию, которая принимает массив стилей и текст, и выводит этот текст с помощью document.write() в тегах <p></p>, добавив в открывающий тег атрибут style со всеми стилями, перечисленными в массиве.
+//Создать объект класса EmpTable и вывести на экран результат работы метода getHtml().
 
-let style = [
-  {
-    option: "width",
-    value: "250px",
-  },
-  {
-    option: "padding",
-    value: "20px 0"
-  },
-  {
-    option: "border-radius",
-    value: "10px"
-  },
-  {
-    option: "background-color",
-    value: "#3584dd",
-  },
-  {
-    option: "color",
-    value: "#e7e7e7",
-  },
-  {
-    option: "font-size",
-    value: "35px",
-  },
-  {
-    option: "text-align",
-    value: "center",
-  },
-  {
-    option: "text-decoration",
-    value: "underline",
-  },
-];
-console.log(style);
-function out(arr, text) {
-  let style = arr.reduce((akm, item) => akm += item.option + ':' + item.value + ';', '');
-  document.write(`<p style = "${style}" > ${text}</p> `);
-}
-out(style, "Перший тест")
-
-//? 4. Создать массив аудиторий академии. Объект-аудитория состоит из названия, количества посадочных мест (от 10 до 20) и названия факультета, для которого она предназначена. Написать несколько функций для работы с ним^
-
-let dep1 = 'Department of Metrology and Information-Measuring Technology';
-let dep2 = 'Department of Software Engineering';
-let dep3 = 'Department of Computer Systems and Networks';
-
-let auditory = [
-  {
-    name: "Home",
-    numberOFseats: 10,
-    department: dep1,
-  },
-  {
-    name: "Aquarium",
-    numberOFseats: 17,
-    department: dep2,
-  },
-  {
-    name: "Horizon",
-    numberOFseats: 14,
-    department: dep3,
-  },
-
-  {
-    name: "Scientist",
-    numberOFseats: 18,
-    department: dep1,
-  },
-];
-
-//todo Вывод на экран всех аудиторий;
-function showAll(arr) {
-  document.write(`<ol>`);
-  arr.forEach((item) => {
-    document.write(`<li> ${item.name}</li > `);
-  })
-  document.write(`</ol > `);
-}
-document.write(`Вывод на экран всех аудиторий; `);
-showAll(auditory);
-
-//todo Вывод на экран аудиторий для указанного факультета;
-function showOfDep(arr, dep) {
-  document.write(`<ol>`);
-  arr.forEach((item) => {
-    if (dep === item.department) document.write(`<li> ${item.name}</ li> `);
-  })
-  document.write(`</ol > `);
-}
-document.write(`Вывод на экран аудиторий для указанного факультета; `);
-showOfDep(auditory, 'Department of Computer Systems and Networks');
-
-//todo Вывод на экран только тех аудиторий, которые подходят для переданной группы. Объект-группа состоит из названия, количества студентов и названия факультета;
-let group1 = { name: 'MVT-09-1', numStudents: 16, ofDep: 'Department of Metrology and Information-Measuring Technology', };
-
-function showOfGroup(arr, group) {
-  document.write(`<ol>`);
-  arr.forEach((item) => {
-    if (group.numStudents <= item.numberOFseats && group.ofDep === item.department) document.write(`<li> ${item.name}</li > `);
-  })
-  document.write(`</ol > `);
-}
-document.write(`Вывод на экран только тех аудиторий, которые подходят для переданной группы.Объект - группа состоит из названия, количества студентов и названия факультета; `);
-showOfGroup(auditory, group1)
-
-//todo Функция сортировки аудиторий по количеству мест;
-function shownumberOFseats0to9(arr) {
-  let sortarr = arr.sort(function (a, b) {
-    return a.numberOFseats - b.numberOFseats;
-  })
-  showAll(sortarr);
-}
-document.write(`Функция сортировки аудиторий по количеству мест; `);
-shownumberOFseats0to9(auditory);
-
-//todo Функция сортировки аудиторий по названию (по алфавиту).
-function showauditoryAtoZ(arr) {
-  let sortarr = arr.sort(function (a, b) {
-    return a.name - b.name;
-  })
-  showAll(sortarr);
-}
-document.write(`Функция сортировки аудиторий по названию(по алфавиту).`);
-showauditoryAtoZ(auditory);
-
+let empTable = new EmpTable(EmployeeOfBank);
+empTable.getHtml;
