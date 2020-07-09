decf589f1966c26ade1765fee922cdb477acd2a7
iSerrr <tsiutsiak.l@gmail.com>
2020-07-09 22:59:06 +0300

    slider it's ready

--- js/main.js
+++ js/main.js
@@ -1,38 +1,22 @@
-$('.game-sale__nav-link').click( function(){
-    event.preventDefault();
-    
-    $('.game-sale__nav-link').removeClass('game-sale__nav-link-active');
-    $(this).addClass('game-sale__nav-link-active');
-
-    
-    $('.game-sale__content').removeClass('game-sale__content-active');
-    $(`.${$(this).attr('href')}`).addClass('game-sale__content-active');
-});
-
-$('.ratting__star').click( function(){
-    $('.ratting__star').removeClass('ratting__star-active');
-    $(this).addClass('ratting__star-active').nextAll().addClass('ratting__star-active');
-});
-
-let i = 0;
-function start() {
-    
-    let b = 220 - (220 / 120 * i);
-    i++;
-
-    $('.progres-bar__svg').css("stroke-dashoffset" , b);
-    $('.progres-bar__text').text(i / 10);
-
-    if (i === 20) clearTimeout(timer);
+let page1 = {
+    title: "We craft meaningful digital experiences ",
+    subTitle: "Creating products and services that transform organizations for the digital economy",
+    button: "SEE OUR WORK",
+    colorBg: "red",
+    colorBtn: "yellow"
 };
 
-function progresBar() {
-    i = 0;
-    let timer;
-    start();
-    
-};
+var el1 = $("div");
 
-$('.progres-bar').click(function () {
-    timer = setInterval(progresBar, 50);
-});
+console.log(el1);
+
+
+
+
+
+
+
+
+
+
+
