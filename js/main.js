b57eacb5585fcc16c3a038b64061b8610f689e0c
iSerrr <tsiutsiak.l@gmail.com>
2020-07-14 21:47:26 +0300

    H34-work, need add interval function

--- js/main.js
+++ js/main.js
@@ -1,41 +1,74 @@
-// // Initialize and add the map
-// function initMap() {
-//     // The location of Uluru
-//     var uluru = {lat: 50.132538, lng: 18.352870};
-//     // The map, centered at Uluru
-//     var map = new google.maps.Map(
-//         document.getElementById('map'), {zoom: 5, center: uluru});
-//     // The marker, positioned at Uluru
-//   }
+let beetroot = [
+    {
+        name:'Beetroot Academy Rivne',
+        lat: 50.619848,
+        lng: 26.249555
+    },
+    {
+        name:'Beetroot Academy Ivano-Frankivsk',
+        lat: 48.924292,
+        lng: 24.721024
+    },    
+    {
+        name:'Beetroot Academy Vinnytsia',
+        lat: 49.219037,
+        lng: 28.436688
+    },
+    {
+        name: 'Beetroot Academy Kiev',
+        lat:50.463918,
+        lng: 30.499892
+    },
+    {
+        name: 'Beetroot Academy Zaporizhia',
+        lat: 47.839188, 
+        lng: 35.122219
+    },
+]
+
+beetroot.forEach(element => {
+    let op = document.createElement('option');
+    op.innerText = element.name;
+    op.value = `{"lat": ${element.lat}, "lng": ${element.lng}}`;
+    op.setAttribute('data-lat', element.lat);
+    op.setAttribute('data-lng', element.lng);
+    $('.select').append(op);
+});
 
 
-      function initMap() {
-        var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 5,
-          center: {lat: 50.132538, lng: 18.352870}
-        });
-        directionsDisplay.setMap(map);
 
-        function onChangeHandler() {
-          calculateAndDisplayRoute(directionsService, directionsDisplay);
-        };
-        onChangeHandler();
-        console.table(directionsService);
-      }
+function initMap() {
+    let uluru = {lat: 48.924292,lng: 24.721024};
+    let map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 6, center: uluru});
+       
+        beetroot.forEach(element => {
+            let marker = new google.maps.Marker({
+                position: {lat: element.lat, lng: element.lng},
+                map: map,
+                title: element.name
+        })
+        marker.addListener("click", function() {
+                map.setZoom(9);
+                map.setCenter(this.getPosition());
+                console.log(this.getPosition());
+        });   
+    })
+    $('.select').change(function() {
+        map.setZoom(9);
+        map.setCenter(JSON.parse($('.select').val()));
+    })
+}
 
-      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        directionsService.route({
-          origin: 'Kiev',
-          destination: 'Wroclaw',
-          travelMode: 'DRIVING'
-        }, function(response, status) {
-          if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      }
-    
+
+
+
+function name(start, end) {
+    let i = start;
+    if (i == and) return;
+    function zoom() {
+        map.setZoom(i);
+        i++;
+    }
+    zoom();
+}
