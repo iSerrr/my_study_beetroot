e049bff1cac8f5e89b24e515bc3514d0086cd75a
iSerrr <tsiutsiak.l@gmail.com>
2020-07-14 19:13:32 +0300

    before class-work

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
-$(document).ready(function(){
-    $('.services__slide-conteiner').slick({
-        dots: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        variableWidth: true,
-        autoplay: true,
-        prevArrow: '<div class="circle"></div>',
-        nextArrow: '<div class="circle circle--right"></div>',
-        dots: false,
-        autoplaySpeed: 3000
-    });
-});
+// // Initialize and add the map
+// function initMap() {
+//     // The location of Uluru
+//     var uluru = {lat: 50.132538, lng: 18.352870};
+//     // The map, centered at Uluru
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), {zoom: 5, center: uluru});
+//     // The marker, positioned at Uluru
+//   }
 
+
+      function initMap() {
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 5,
+          center: {lat: 50.132538, lng: 18.352870}
+        });
+        directionsDisplay.setMap(map);
+
+        function onChangeHandler() {
+          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        };
+        onChangeHandler();
+        console.table(directionsService);
+      }
+
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+          origin: 'Kiev',
+          destination: 'Wroclaw',
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
+    
