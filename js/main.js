19c1aabc2b4c7a10e0e41982bbf9e7cfc5b213af
iSerrr <tsiutsiak.l@gmail.com>
2020-07-15 00:12:03 +0300

    H34 ready 100%

--- js/main.js
+++ js/main.js
@@ -1,41 +1,34 @@
-// // Initialize and add the map
-// function initMap() {
-//     // The location of Uluru
-//     var uluru = {lat: 50.132538, lng: 18.352870};
-//     // The map, centered at Uluru
-//     var map = new google.maps.Map(
-//         document.getElementById('map'), {zoom: 5, center: uluru});
-//     // The marker, positioned at Uluru
-//   }
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    mapId: "16ee99ccaa8fd99f",
+    zoom: 5,
+    center: {lat: 50.132538, lng: 18.352870},
+    disableDefaultUI: true,
+  });
+  directionsDisplay.setMap(map);
 
+  function onChangeHandler() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  $('.createRoute').click(function() {
+    console.log(directionsService);
+    onChangeHandler();
+  });  
+}
 
-      function initMap() {
-        var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 5,
-          center: {lat: 50.132538, lng: 18.352870}
-        });
-        directionsDisplay.setMap(map);
-
-        function onChangeHandler() {
-          calculateAndDisplayRoute(directionsService, directionsDisplay);
-        };
-        onChangeHandler();
-        console.table(directionsService);
-      }
-
-      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        directionsService.route({
-          origin: 'Kiev',
-          destination: 'Wroclaw',
-          travelMode: 'DRIVING'
-        }, function(response, status) {
-          if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      }
-    
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: $('.start').val(),
+    destination: $('.finish').val(),
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Ввдено не вірні дані, спробуйте ще раз');
+    }
+  });
+  console.table(directionsService);
+}  
