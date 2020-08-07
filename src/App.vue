6cf040f2c5d7023898a0b64a579e31f9601e1c07
iSerrr <tsiutsiak.l@gmail.com>
2020-08-07 13:31:07 +0300

    ready to work, but need fix

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <StartPage/>
+  </div>
+ 
+</template>
+
+<script>
+import StartPage from './components/StartPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    StartPage,
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100vh;
+}
+</style>
+
+<style lang="scss">
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/StartPage.vue
@@ -0,0 +1,34 @@
+<template>
+	<section class="vue-main">
+		<h1 class="vue-main__title">Vue.js</h1>
+		<p class="vue-main__desc">this is start page with Vue.js</p>
+	</section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.vue-main {
+	transition: all 500ms ease;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: 100%;
+	background-color: rgb(189, 189, 189);
+	font-family: sans-serif;
+	text-shadow: 0 0 5px rgba(0, 0, 0, 0.35);
+	&__title {
+		font-size: 75px;
+		color: rgb(116, 253, 208);
+		
+	}
+	&__desc {
+		font-size: 55px;
+		color: rgb(101, 255, 204);
+	}
+}
+</style>
